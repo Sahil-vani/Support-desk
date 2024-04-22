f6161e029c53b8ec7b82ff09ef85b1d58bbf9399
sahil vani <135162871+Sahil-vani@users.noreply.github.com>
2024-04-22 18:53:04 +0530

    Updated proxy url in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://support-desk-7hlh.onrender.com",
+        target: "http://localhost:9000",
         changeOrigin: true,
         secure: false,
       },
