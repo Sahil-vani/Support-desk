94bd2aaa9c5400fdaa5542b0f30df5b98343e07f
sahil vani <135162871+Sahil-vani@users.noreply.github.com>
2024-04-23 14:28:35 +0530

    removing proxy from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "https://support-desk-7hlh.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://support-desk-7hlh.onrender.com",
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
 });
